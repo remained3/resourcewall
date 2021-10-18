5929f408bf2bda02f9b7fb46780414b6b8706d15
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-10-18 20:41:33 +0000

    Split database schema (table definitions) and seeds (inserts) into separate files, one per table

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS resources CASCADE;
-DROP TABLE IF EXISTS comments CASCADE;
-DROP TABLE IF EXISTS resource_rates CASCADE;
-DROP TABLE IF EXISTS resource_likes CASCADE;
+-- DROP TABLE IF EXISTS resources CASCADE;
+-- DROP TABLE IF EXISTS comments CASCADE;
+-- DROP TABLE IF EXISTS resource_rates CASCADE;
+-- DROP TABLE IF EXISTS resource_likes CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -14,34 +14,34 @@ CREATE TABLE users (
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE resources (
-  id SERIAL PRIMARY KEY NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  description TEXT,
-  thumbnail_photo_url VARCHAR(255) NOT NULL,
-  cover_photo_url VARCHAR(255) NOT NULL,
-  topic VARCHAR(255) NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  creation_date DATE
-);
+-- CREATE TABLE resources (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   title VARCHAR(255) NOT NULL,
+--   description TEXT,
+--   thumbnail_photo_url VARCHAR(255) NOT NULL,
+--   cover_photo_url VARCHAR(255) NOT NULL,
+--   topic VARCHAR(255) NOT NULL,
+--   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+--   creation_date DATE
+-- );
 
-CREATE TABLE comments (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
-  comment TEXT,
-);
+-- CREATE TABLE comments (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+--   resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+--   comment TEXT,
+-- );
 
-CREATE TABLE resource_rates (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
-  rating SMALLINT NOT NULL DEFAULT 0,
-);
+-- CREATE TABLE resource_rates (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+--   resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+--   rating SMALLINT NOT NULL DEFAULT 0,
+-- );
 
-CREATE TABLE resource_likes (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
-  like SMALLINT NOT NULL DEFAULT 0,
-);
+-- CREATE TABLE resource_likes (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+--   resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+--   like SMALLINT NOT NULL DEFAULT 0,
+-- );
