0974102f096e566dc098fba20eff7b803b554efd
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-10-19 16:07:39 +0000

    routes ready

--- routes/likes.js
+++ routes/likes.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    let query = `SELECT * FROM likes`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const likes = data.rows;
+        res.json({ likes });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
