6b5d5c0490d4f94185e35f00d161cc59ad135833
remained3 <sarahthebush@hotmail.com>
2021-10-20 11:06:29 +0000

    added example data

---
+++ db/seeds/02_resources.sql
@@ -0,0 +1,12 @@
+INSERT INTO resources (title, description, thumbnail_photo_url, cover_photo_url, topic, user_id)
+VALUES
+('Light1', 'help', 'link', 'pic', 'webdev', '1'),
+('Light2', 'help', 'link', 'pic', 'webdev', '2'),
+('Light3', 'help', 'link', 'pic', 'webdev', '1'),
+('Light4', 'help', 'link', 'pic', 'webdev', '1'),
+('Light5', 'help', 'link', 'pic', 'webdev', '1'),
+('Light6', 'help', 'link', 'pic', 'webdev', '2'),
+('Light7', 'help', 'link', 'pic', 'webdev', '1'),
+('Light8', 'help', 'link', 'pic', 'webdev', '2'),
+('Light9', 'help', 'link', 'pic', 'webdev', '2'),
+
--- migrations/02_schema_resources.sql
+++ migrations/02_schema_resources.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS resources CASCADE;
 CREATE TABLE resources (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
+  url VARCHAR(255) NOT NULL,
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
